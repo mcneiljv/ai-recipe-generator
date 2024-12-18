64cb509fdb18f43d9f93fd021bd0ebbd65bef613
Jamie McNeil <jamievmcneil@gmail.com>
2024-12-18 09:13:54 -0500

    dangerouslyAllowBrowser for now; Add catch to fetchRecipe

--- src/api/fetchRecipe.tsx
+++ src/api/fetchRecipe.tsx
@@ -1,35 +1,39 @@
 import OpenAI from 'openai';
 
 const client = new OpenAI({
-  apiKey: import.meta.env.VITE_OPEN_AI_KEY
+  apiKey: import.meta.env.VITE_OPEN_AI_KEY,
+  dangerouslyAllowBrowser: true
 });
 
 // TODO: Add types for props
 export const fetchRecipe = async (ingredients: any, filters: any) => {
-  console.log('test');
+  const chatCompletion: OpenAI.Chat.Completions.ChatCompletion =
+    await client.chat.completions
+      .create({
+        max_tokens: 200,
+        messages: [
+          {
+            role: 'system',
+            content:
+              'You are a helpful assistant that generates recipes based on given ingredients and filters.'
+          },
+          {
+            role: 'user',
+            content: `
+            I have the following ingredients: ${ingredients.join(', ')}.
+            Please suggest a recipe considering these filters:
+            - Cuisine: ${filters.cuisine || 'any'}
+            - Dietary Restrictions: ${filters.dietary || 'none'}
+            - Cooking Time: ${filters.time || 'any'} minutes.
+          `
+          }
+        ],
+        model: 'gpt-3.5-turbo-16k'
+      })
+      .catch((error: Error) => {
+        console.error('Error fetching recipe:', error);
+        throw new Error('Failed to fetch recipe. Please try again.');
+      });
 
-  //   const chatCompletion: OpenAI.Chat.Completions.ChatCompletion =
-  //     await client.chat.completions.create({
-  //       max_tokens: 200,
-  //       messages: [
-  //         {
-  //           role: 'system',
-  //           content:
-  //             'You are a helpful assistant that generates recipes based on given ingredients and filters.'
-  //         },
-  //         {
-  //           role: 'user',
-  //           content: `
-  //           I have the following ingredients: ${ingredients.join(', ')}.
-  //           Please suggest a recipe considering these filters:
-  //           - Cuisine: ${filters.cuisine || 'any'}
-  //           - Dietary Restrictions: ${filters.dietary || 'none'}
-  //           - Cooking Time: ${filters.time || 'any'} minutes.
-  //         `
-  //         }
-  //       ],
-  //       model: 'gpt-3.5-turbo-16k'
-  //     });
-
-  //   return chatCompletion.choices[0].message.content;
+  return chatCompletion.choices[0].message.content;
 };

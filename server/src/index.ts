8104ccec31946d4e9dbaabeec64fa9a2af89403b
Jamie McNeil <jamievmcneil@gmail.com>
2024-12-20 14:31:56 -0500

    Add router and env vars

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,31 @@
-import express from 'express';
+import express, { Application } from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+// import dotenv from 'dotenv';
+import OpenAI from 'openai';
+import { recipeRoutes } from './routes';
 
-const app = express();
-const port = 3000;
+// dotenv.config();
 
+const app: Application = express();
+const port: number = 3000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// OpenAI Configuration
+const openai = new OpenAI({
+  apiKey: process.env.OPEN_AI_KEY
+});
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Express + TypeScript Server');
+});
+app.use('/api', recipeRoutes);
+
+// Start server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });

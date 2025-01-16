88bd1b926d694f731c8508c822d27cc633c67aa3
Jamie McNeil <jamievmcneil@gmail.com>
2025-01-16 08:55:30 -0500

    Begin adding services config

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,19 @@
 import express, { Application } from 'express';
-import bodyParser from 'body-parser';
 import cors from 'cors';
-// import dotenv from 'dotenv';
 import OpenAI from 'openai';
 import { recipeRoutes } from './routes';
-
-// dotenv.config();
 
 const app: Application = express();
 const port: number = 3000;
 
 // Middleware
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
+
+// Environment variable validation
+if (!process.env.OPEN_AI_KEY) {
+  throw new Error('Missing OPEN_AI_KEY environment variable');
+}
 
 // OpenAI Configuration
 const openai = new OpenAI({
@@ -24,6 +25,19 @@ app.get('/', (req, res) => {
   res.send('Express + TypeScript Server');
 });
 app.use('/api', recipeRoutes);
+
+// Error handling middleware
+app.use(
+  (
+    err: Error,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    console.error(err.stack);
+    res.status(500).send({ error: 'Something went wrong!' });
+  }
+);
 
 // Start server
 app.listen(port, () => {
